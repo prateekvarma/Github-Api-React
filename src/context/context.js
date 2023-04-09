b10d242d791b1823974c5593ccc3322d3bfcae61
prateekvarma <prtk.varma@gmail.com>
2023-04-09 11:59:55 -0600

    Programming the spinner's conditions

--- src/context/context.js
+++ src/context/context.js
@@ -13,10 +13,12 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
   const [requests, setRequests] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState({ show: false, msg: '' });
 
   const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
       console.error(err)
     );
@@ -25,6 +27,8 @@ const GithubProvider = ({ children }) => {
     } else {
       toggleError(true, 'there is no user with that username');
     }
+    checkRequests();
+    setIsLoading(false);
   };
 
   const checkRequests = () => {
