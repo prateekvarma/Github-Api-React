2aea01bbe751de498becb02e1236f4c97fb43c41
prateekvarma <prtk.varma@gmail.com>
2023-04-13 13:09:22 -0600

    Adding Auth0 provider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+  <React.StrictMode
+    domain='dev-1wqavlh6rvd3uhns.us.auth0.com'
+    clientId='nXKEWlJRjkz3fEuP0YTPzH7jJ9k2D8LY'
+    redirectUri={window.location.origin}
+  >
+    <Auth0Provider>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
