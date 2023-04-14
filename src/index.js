9156bb68615e60aeff71c43d201de4f0c0360410
prateekvarma <prtk.varma@gmail.com>
2023-04-14 13:26:36 -0600

    Login button on navbar for auth0

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode
-    domain='dev-1wqavlh6rvd3uhns.us.auth0.com'
-    clientId='nXKEWlJRjkz3fEuP0YTPzH7jJ9k2D8LY'
-    redirectUri={window.location.origin}
-  >
-    <Auth0Provider>
+  <React.StrictMode>
+    <Auth0Provider
+      domain='dev-1wqavlh6rvd3uhns.us.auth0.com'
+      clientId='nXKEWlJRjkz3fEuP0YTPzH7jJ9k2D8LY'
+      redirectUri={window.location.origin}
+    >
       <GithubProvider>
         <App />
       </GithubProvider>
